a1d25d8a39f8ab889a32dc32a65d3fa8890300c5
sqyc <songqiaoyuchen@gmail.com>
2026-01-17 23:22:11 +0800

    hi: rename Duke to Ding

---
+++ src/main/java/Ding.java
@@ -0,0 +1,14 @@
+public class Ding {
+    public static void main(String[] args) {
+        String logo = " ____  _ \n"
+                + "|  _ \\(_)_ __   __ _ \n"
+                + "| | | | | '_ \\ / _` |\n"
+                + "| |_| | | | | | (_| |\n"
+                + "|____/|_|_| |_|\\__, |\n"
+                + "               |___/ \n";
+        System.out.println(logo);
+        // greeting and exit msg
+        System.out.println("Hi there! I'm Ding, your personal assistant :)");
+        System.out.println("I am still learning, see you again soon!");
+    }
+}
