ff59776b999c1a6fabb05ef2fbd8c895f9e64dcf
sqyc <songqiaoyuchen@gmail.com>
2026-01-18 18:10:16 +0800

    feat: mark tasks as done/undone
    - add Task class to represent tasks
    - Ding.java main is getting big, should refactor later

--- src/main/java/Ding.java
+++ src/main/java/Ding.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ding {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____  _ \n"
                 + "|  _ \\(_)_ __   __ _ \n"
@@ -24,20 +24,58 @@ public class Ding {
         while (true) {
             System.out.print("You: ");
             String userInput = scanner.nextLine();
+            // exit case
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Ding: Goodbye! We will meet again soon.");
                 break;
+            // empty input
             } else if (userInput.trim().isEmpty()) {
-                System.out.println("Ding: Add some tasks to your to-do list, and I will remember it for you.");
+                System.out.println("Ding: Add some tasks to your to-do list, " 
+                    + "and I will remember it for you.");
+            // list tasks 
             } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("Ding: Here are the tasks you have added:");
                 for (int i = 0; i < tasks.size(); i++) {
                     if (tasks.get(i) != null) {
-                        System.out.println((i + 1) + ". " + tasks.get(i));
+                        Task task = tasks.get(i);
+                        String taskNumber = (i + 1) + ". ";
+                        System.out.println(taskNumber + task.toString());
                     }
                 }
+            // mark task as done or not done
+            } else if (userInput.toLowerCase().startsWith("mark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < tasks.size() 
+                            && tasks.get(taskIndex) != null && !tasks.get(taskIndex).isDone()) {
+                        tasks.get(taskIndex).toggleDone();
+                        System.out.println("Ding: Ok, good job on completing this one :)");
+                        System.out.println(tasks.get(taskIndex).toString());
+                    } else {
+                        System.out.println("Ding: I don't get it. " 
+                            + "This task either don't exist or is already marked as done.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Ding: What is this? Please provide a valid task number.");
+                }
+            } else if (userInput.toLowerCase().startsWith("unmark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                    if (taskIndex >= 0 && taskIndex < tasks.size() 
+                            && tasks.get(taskIndex) != null && tasks.get(taskIndex).isDone()) {
+                        tasks.get(taskIndex).toggleDone();
+                        System.out.println("Ding: OKOK, I've marked this task as not done yet.");
+                        System.out.println(tasks.get(taskIndex).toString());
+                    } else {
+                        System.out.println("Ding: I don't get it. " 
+                            + "This task either don't exist or is already marked as not done.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Ding: What is this? Please provide a valid task number.");
+                }
+            // add new task
             } else {
-                tasks.add(userInput);
+                tasks.add(new Task(userInput));
                 System.out.println("Ding: Got it. I've added this task to your to-do list.");
             }
             System.out.println(LINE_BREAK);
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void toggleDone() {
+        isDone = !isDone;
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
